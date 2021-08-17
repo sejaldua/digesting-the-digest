f5badfd34a3244b69154f63d2f485c1e03593a39
Sejal Dua <sejal.dua@tufts.edu>
2021-08-16 22:02:34 -0400

    done with long notebook

--- quickstart.py
+++ quickstart.py
@@ -8,7 +8,7 @@ from google.oauth2.credentials import Credentials
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 
-def get_data():
+def get_service():
     """Shows basic usage of the Gmail API.
     Lists the user's Gmail labels.
     """
@@ -31,19 +31,39 @@ def get_data():
             token.write(creds.to_json())
 
     service = build('gmail', 'v1', credentials=creds)
-            
+    return service
+
+def get_all_email_labels(service):
+    # print out all labels
+    results = service.users().labels().list(userId='me').execute()
+    labels = results.get('labels', [])
+    if not labels:
+        print('No labels found.')
+    else:
+        print('Labels:')
+        for label in labels:
+            print(label['name'], label['id'])
+        
+def get_data(service, email_type):
+    # set label variable depending on email type we want to look at
+    if email_type == 'Medium Daily Digest':
+        label = 'Label_7938937032304597046'
+    elif email_type == "MIT Download":
+        label = 'Label_4668905010391853459'
+    
     # Call the Gmail API
-    results = service.users().messages().list(userId='me',labelIds = ['Label_7938937032304597046'], maxResults=500).execute()
+    results = service.users().messages().list(userId='me',labelIds = [label], maxResults=500).execute()
     messages = results.get('messages', [])
     nextPageToken = results.get('nextPageToken')
     resultSizeEstimate = results.get('resultSizeEstimate')
     while len(messages) % 500 == 0:
-        results = service.users().messages().list(userId='me',labelIds = ['Label_7938937032304597046'], maxResults=500, pageToken=nextPageToken).execute()
+        results = service.users().messages().list(userId='me',labelIds = [label], maxResults=500, pageToken=nextPageToken).execute()
         new_messages = results.get('messages', [])
         nextPageToken = results.get('nextPageToken')
         resultSizeEstimate = results.get('resultSizeEstimate')
         messages.extend(new_messages)
-    return service, messages
+    return messages
 
 if __name__ == '__main__':
-    get_data()
+    service = get_service()
+    get_all_email_labels(service)
